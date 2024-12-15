c26cec76aea92c9ebf3a366d5745adc7fe6fc4ab
Santeri Kääriäinen <santeri.kaariainen@iki.fi>
2024-12-15 20:20:49 +0200

    No debug logging

--- cmd/gpsp-bot-discord/bot.go
+++ cmd/gpsp-bot-discord/bot.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"log/slog"
-
 	"github.com/napuu/gpsp-bot/internal/platforms"
 )
 
 func main() {
-	slog.SetLogLoggerLevel(slog.LevelDebug)
 	platforms.RunDiscordBot()
 }
--- cmd/gpsp-bot-telegram/bot.go
+++ cmd/gpsp-bot-telegram/bot.go
@@ -5,6 +5,5 @@ import (
 )
 
 func main() {
-	// slog.SetLogLoggerLevel(slog.LevelDebug)
 	platforms.RunTelegramBot()
 }
